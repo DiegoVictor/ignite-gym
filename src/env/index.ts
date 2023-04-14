691bd2143624807b1255fe5cee2be89ba8073ccc
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-13 22:36:04 -0300

    Fix from linters

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,8 @@
-import "dotenv/config";
-import { z } from "zod";
+import 'dotenv/config';
+import { z } from 'zod';
 
 const schema = z.object({
-  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app';
+import { env } from './env';
 
 app
   .listen({
     port: env.PORT,
   })
   .then(() => {
-    console.log("Server is running!");
+    console.log('Server is running!');
   });
