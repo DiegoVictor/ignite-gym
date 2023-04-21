98f274ede8498b0866c4d17e3eed1b7e54d8399a
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-21 20:04:08 -0300

    Implement repository methods

--- src/repositories/prisma/users-repository.ts
+++ src/repositories/prisma/users-repository.ts
@@ -1,11 +1,22 @@
 import { Prisma } from '@prisma/client';
 
 import { prisma } from '@/lib/prisma';
+import { IUsersRepository } from '../users-repository';
 
-export class PrismaUserRepository {
+export class PrismaUserRepository implements IUsersRepository {
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
+    });
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
     });
 
     return user;
