a0f57a9693aff4406ed3a10d67a5d8b773acdc03
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-22 10:02:27 -0300

    Create error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify';
+import { ZodError } from 'zod';
+
 import { routes } from './http/routes';
 
 export const app = fastify();
 
 app.register(routes);
+app.setErrorHandler((err, _, reply) => {
+  if (err instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation Error',
+      issues: err.format(),
+    });
+  }
+
+  if (err.statusCode) {
+    return reply.status(err.statusCode).send({ message: err.message });
+  }
+
+  return reply.status(500).send();
+});
