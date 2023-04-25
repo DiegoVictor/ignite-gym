08ecac8a769e1a9bda5ea37c560dd94ca0d40043
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-25 09:24:44 -0300

    Add sessions route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify';
 
 import { register } from './controllers/register';
+import { authenticate } from './controllers/authenticate';
 
 export async function routes(app: FastifyInstance) {
   app.post('/users', register);
+  app.post('/sessions', authenticate);
 }
