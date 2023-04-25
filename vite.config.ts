b58be579b858423e7472148ec05450132ccdad74
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-25 08:54:35 -0300

    Configure a different coverage results folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      reportsDirectory: 'tests/coverage',
+    },
+  },
 });
