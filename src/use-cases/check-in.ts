4c45f3dd52eaaf28abf64048b31965463ef62c1a
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-28 00:18:58 -0300

    Prevent to check in twice in a day

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -17,6 +17,14 @@ export class CheckInUseCase {
     userId,
     gymId,
   }: ICheckInUseCaseRequest): Promise<ICheckInUseCaseResponse> {
+    const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date()
+    );
+    if (checkInOnSameDay) {
+      throw new Error();
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId,
