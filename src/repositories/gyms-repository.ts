bbd6ec00ab6a1a4cc66c0d4143037558d76c32f1
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-30 11:46:39 -0300

    Add create gym repository method

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,5 +1,6 @@
 import { IGym } from '@/contracts/gym';
 
 export interface IGymsRepository {
+  create(data: IGym): Promise<IGym>;
   findById(id: string): Promise<IGym | null>;
 }
--- src/repositories/in-memory/gyms-repository.ts
+++ src/repositories/in-memory/gyms-repository.ts
@@ -1,8 +1,25 @@
+import { randomUUID } from 'node:crypto';
+
 import { IGymsRepository } from '../gyms-repository';
 import { IGym } from '@/contracts/gym';
 
 export class InMemoryGymsRepository implements IGymsRepository {
   public gyms: IGym[] = [];
+
+  public async create(data: IGym) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      name: data.name,
+      description: data.description,
+      phone: data.phone,
+      latitude: data.latitude,
+      longitude: data.longitude,
+    };
+
+    this.gyms.push(gym);
+
+    return gym;
+  }
 
   public async findById(id: string): Promise<IGym | null> {
     const gym = this.gyms.find(gym => gym.id === id);
