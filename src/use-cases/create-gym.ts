fcb0e50bdfff850f03cd900f16a10180263f58ab
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-30 11:47:39 -0300

    Create use case to add a new gym

---
+++ src/use-cases/create-gym.ts
@@ -0,0 +1,36 @@
+import { IGym } from '@/contracts/gym';
+import { IGymsRepository } from '@/repositories/gyms-repository';
+
+interface ICreateGymRequest {
+  name: string;
+  description: string | null;
+  phone: string | null;
+  latitude: number;
+  longitude: number;
+}
+
+interface ICreateGymResponse {
+  gym: IGym;
+}
+
+export class CreateGymUseCase {
+  constructor(private gymsRepository: IGymsRepository) {}
+
+  public async execute({
+    name,
+    description,
+    phone,
+    latitude,
+    longitude,
+  }: ICreateGymRequest): Promise<ICreateGymResponse> {
+    const gym = await this.gymsRepository.create({
+      name,
+      description,
+      phone,
+      latitude,
+      longitude,
+    });
+
+    return { gym };
+  }
+}
