1e9c8040428dd0ec5263951207e7f5aaeaee1209
Diego Victor <diegovictorgonzaga@gmail.com>
2023-05-01 10:45:35 -0300

    Test get user history use case

---
+++ src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,77 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/check-ins-repository';
+import { faker } from '@faker-js/faker';
+import { FetchUserCheckInsHistoryUseCase } from './fetch-user-check-ins-history';
+import { PAGINATION_LIMIT } from '@/utils/constants';
+
+let repository: InMemoryCheckInsRepository;
+let fetchUserCheckInsHistoryUseCase: FetchUserCheckInsHistoryUseCase;
+
+describe('Fetch User Check Ins History Use Case', () => {
+  beforeEach(() => {
+    repository = new InMemoryCheckInsRepository();
+    fetchUserCheckInsHistoryUseCase = new FetchUserCheckInsHistoryUseCase(
+      repository
+    );
+  });
+
+  it('should be able to fetch user check-ins history', async () => {
+    const userId = faker.datatype.uuid();
+    const gymIds = Array.from({ length: 2 }, () => faker.datatype.uuid());
+
+    await Promise.all(
+      gymIds.map(gymId =>
+        repository.create({
+          user_id: userId,
+          gym_id: gymId,
+        })
+      )
+    );
+
+    const { checkIns } = await fetchUserCheckInsHistoryUseCase.execute({
+      userId,
+    });
+
+    expect(checkIns).toHaveLength(2);
+    expect(checkIns).toEqual(
+      gymIds.map(gymId =>
+        expect.objectContaining({
+          user_id: userId,
+          gym_id: gymId,
+        })
+      )
+    );
+  });
+
+  it('should be able to fetch paginated check-ins history', async () => {
+    const userId = faker.datatype.uuid();
+    const gymIds = Array.from({ length: PAGINATION_LIMIT + 2 }, () =>
+      faker.datatype.uuid()
+    );
+
+    await Promise.all(
+      gymIds.map(gymId =>
+        repository.create({
+          user_id: userId,
+          gym_id: gymId,
+        })
+      )
+    );
+
+    const { checkIns } = await fetchUserCheckInsHistoryUseCase.execute({
+      userId,
+      page: 2,
+    });
+
+    expect(checkIns).toHaveLength(2);
+    expect(checkIns).toEqual(
+      gymIds.slice(-2).map(() =>
+        expect.objectContaining({
+          user_id: userId,
+          gym_id: expect.any(String),
+        })
+      )
+    );
+  });
+});
