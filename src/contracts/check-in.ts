63cf4b4b606890b56c90477c36b5a194d67729e6
Diego Victor <diegovictorgonzaga@gmail.com>
2023-05-05 00:05:26 -0300

    Not allow null created_at

--- src/contracts/check-in.ts
+++ src/contracts/check-in.ts
@@ -3,7 +3,7 @@ export interface ICheckIn {
   user_id: string;
   gym_id: string;
   validated_at?: Date | string | null | undefined;
-  created_at?: Date | string | null | undefined;
+  created_at?: Date | string | undefined;
 }
 
 export interface ICheckInsRepository {
