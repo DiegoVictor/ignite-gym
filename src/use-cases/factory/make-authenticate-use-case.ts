a215d444c87feead70c90c81eae1007eeed69e34
Diego Victor <diegovictorgonzaga@gmail.com>
2023-05-06 10:35:58 -0300

    Fix import

--- src/use-cases/factory/make-authenticate-use-case.ts
+++ src/use-cases/factory/make-authenticate-use-case.ts
@@ -1,8 +1,8 @@
-import { PrismaUserRepository } from '@/repositories/prisma/users-repository';
+import { PrismaUsersRepository } from '@/repositories/prisma/users-repository';
 import { AuthenticateUseCase } from '../authenticate';
 
 export function makeAuthenticateUseCase(): AuthenticateUseCase {
-  const usersRepository = new PrismaUserRepository();
+  const usersRepository = new PrismaUsersRepository();
   const authenticateUserUseCase = new AuthenticateUseCase(usersRepository);
 
   return authenticateUserUseCase;
