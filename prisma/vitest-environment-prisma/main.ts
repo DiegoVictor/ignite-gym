f3bff4bdcf4a5f3745410d44e218fc092299c093
Diego Victor <diegovictorgonzaga@gmail.com>
2023-05-08 09:29:27 -0300

    Create new schema for each test suite

--- prisma/vitest-environment-prisma/main.ts
+++ prisma/vitest-environment-prisma/main.ts
@@ -1,13 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
 import { Environment } from 'vitest';
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error(
+      'DATABASE_URL environment variable should at least has an initial value'
+    );
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Setup');
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseUrl(schema);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync(`npx prisma migrate deploy`);
 
     return {
-      teardown() {
-        console.log('Teardown');
+      async teardown() {
+        const prisma = new PrismaClient();
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE;`
+        );
+        await prisma.$disconnect();
       },
     };
   },
