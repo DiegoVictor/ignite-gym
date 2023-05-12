5ece9d2311eb478465df84a00f08b6fb72639fe4
Diego Victor <diegovictorgonzaga@gmail.com>
2023-05-12 09:51:14 -0300

    Validate a check-in

---
+++ src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,19 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+import { makeValidateCheckInUseCase } from '@/use-cases/factory/make-validate-check-in-use-case';
+
+export async function validate(request: FastifyRequest, reply: FastifyReply) {
+  const schema = z.object({
+    checkInId: z.string(),
+  });
+
+  const { checkInId } = schema.parse(request.params);
+
+  const validateCheckInUseCase = makeValidateCheckInUseCase();
+  await validateCheckInUseCase.execute({
+    checkInId,
+  });
+
+  return reply.status(204).send();
+}
