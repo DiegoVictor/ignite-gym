ec2a2c8c2b14cf1bbef35987c9d7989e1de633a3
Diego Victor <diegovictorgonzaga@gmail.com>
2023-05-12 09:50:09 -0300

    Test gyms search

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,51 @@
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+
+import { app } from '@/app';
+import { createUserAndAuthenticate } from 'tests/jwt';
+import { factory } from 'tests/factory';
+import { IGym } from '@/contracts/gym';
+
+describe('Nearby Gyms Controller', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to list nearby gyms', async () => {
+    const { token } = await createUserAndAuthenticate(app);
+
+    const gyms = Array.from({ length: 2 }, () => factory.attrs<IGym>('Gym'));
+
+    await Promise.all(
+      gyms.map(gym =>
+        request(app.server)
+          .post('/gyms')
+          .set('Authorization', `Bearer ${token}`)
+          .send(gym)
+      )
+    );
+
+    const [gym] = gyms;
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: gym.latitude,
+        longitude: gym.longitude,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.gyms).toHaveLength(1);
+    expect(response.body.gyms).toContainEqual({
+      ...gym,
+      id: expect.any(String),
+      latitude: Number(gym.latitude),
+      longitude: Number(gym.longitude),
+    });
+  });
+});
---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,50 @@
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+
+import { app } from '@/app';
+import { createUserAndAuthenticate } from 'tests/jwt';
+import { factory } from 'tests/factory';
+import { IGym } from '@/contracts/gym';
+
+describe('Search Gyms Controller', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to search gyms by name', async () => {
+    const { token } = await createUserAndAuthenticate(app);
+
+    const gyms = Array.from({ length: 3 }, () => factory.attrs<IGym>('Gym'));
+
+    await Promise.all(
+      gyms.map(({ name, description, latitude, longitude, phone }) =>
+        request(app.server)
+          .post('/gyms')
+          .set('Authorization', `Bearer ${token}`)
+          .send({ name, description, latitude, longitude, phone })
+      )
+    );
+
+    const [gym] = gyms;
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        q: gym.name,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.gyms).toHaveLength(1);
+    expect(response.body.gyms).toContainEqual({
+      ...gym,
+      id: expect.any(String),
+      latitude: Number(gym.latitude),
+      longitude: Number(gym.longitude),
+    });
+  });
+});
