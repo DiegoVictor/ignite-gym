31309b460c48d813d56160b00e3ca606da8c14ce
Diego Victor <diegovictorgonzaga@gmail.com>
2023-05-12 09:51:47 -0300

    Test check-in validation

---
+++ src/http/controllers/check-ins/validate.spec.ts
@@ -0,0 +1,61 @@
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+
+import { app } from '@/app';
+import { authenticate } from 'tests/jwt';
+import { factory } from 'tests/factory';
+import { IGym } from '@/contracts/gym';
+import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
+import { IUser } from '@/contracts/user';
+
+describe('Validate Check-In Controller', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to validate a check in', async () => {
+    const user = factory.attrs<Required<IUser>>('User');
+    const gym = factory.attrs<Required<IGym>>('Gym');
+
+    await Promise.all([
+      prisma.gym.create({ data: gym }),
+      hash(user.password, 6).then(password =>
+        prisma.user.create({
+          data: {
+            ...user,
+            password,
+          },
+        })
+      ),
+    ]);
+
+    const [token, { id: checkInId }] = await Promise.all([
+      authenticate(app, user.email, user.password),
+      prisma.checkin.create({
+        data: {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+      }),
+    ]);
+
+    const response = await request(app.server)
+      .patch(`/check-ins/${checkInId}/validate`)
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toBe(204);
+
+    const checkIn = await prisma.checkin.findFirst({
+      where: {
+        id: checkInId,
+      },
+    });
+    expect(checkIn).toHaveProperty('validated_at', expect.any(Date));
+  });
+});
