8575eb2b127fe7ca670ec91d6b9e0f1384545441
Diego Victor <diegovictorgonzaga@gmail.com>
2023-05-13 13:40:09 -0300

    Return refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -23,7 +23,25 @@ export async function authenticate(
     }
   );
 
-  return reply.status(200).send({
-    token,
-  });
+  const refreshToken = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: user.id,
+        expiresIn: '7d',
+      },
+    }
+  );
+
+  return reply
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({
+      token,
+    });
 }
