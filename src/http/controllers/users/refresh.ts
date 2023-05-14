994f04a9d95c43d32473cb7aa890d3986f1e6b1f
Diego Victor <diegovictorgonzaga@gmail.com>
2023-05-14 09:20:43 -0300

    Create refresh token endpoint

---
+++ src/http/controllers/users/refresh.ts
@@ -0,0 +1,35 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify({ onlyCookie: true });
+
+  const { sub: userId, role } = request.user;
+  const token = await reply.jwtSign(
+    { role },
+    {
+      sub: userId,
+    }
+  );
+
+  const refreshToken = await reply.jwtSign(
+    { role },
+    {
+      sign: {
+        sub: userId,
+        expiresIn: '7d',
+      },
+    }
+  );
+
+  return reply
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({
+      token,
+    });
+}
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,10 +4,13 @@ import { register } from './register';
 import { authenticate } from './authenticate';
 import { profile } from './profile';
 import { verifyJWT } from '../../middlewares/verify-jwt';
+import { refresh } from './refresh';
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refresh);
 
   app.get('/me', { onRequest: [verifyJWT] }, profile);
 }
