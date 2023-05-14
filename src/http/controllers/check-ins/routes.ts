fa1f7758dccbd66b25353cde685143700267d6a9
Diego Victor <diegovictorgonzaga@gmail.com>
2023-05-14 09:22:05 -0300

    Prevent members to access endpoints

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -5,12 +5,18 @@ import { create } from './create';
 import { validate } from './validate';
 import { history } from './history';
 import { metrics } from './metrics';
+import { verifyUserRole } from '@/http/middlewares/verify-user-role';
+import { USER_ROLE } from '@/contracts/user';
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT);
 
   app.post('/gyms/:gymId/check-ins', create);
-  app.patch('/check-ins/:checkInId/validate', validate);
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: [verifyUserRole(USER_ROLE.ADMIN)] },
+    validate
+  );
 
   app.get('/check-ins/history', history);
   app.get('/check-ins/metrics', metrics);
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -4,11 +4,13 @@ import { verifyJWT } from '../../middlewares/verify-jwt';
 import { search } from './search';
 import { nearby } from './nearby';
 import { create } from './create';
+import { verifyUserRole } from '@/http/middlewares/verify-user-role';
+import { USER_ROLE } from '@/contracts/user';
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT);
 
-  app.post('/gyms', create);
+  app.post('/gyms', { onRequest: [verifyUserRole(USER_ROLE.ADMIN)] }, create);
 
   app.get('/gyms/nearby', nearby);
   app.get('/gyms/search', search);
